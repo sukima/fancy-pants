5002ccf913ca0871c24f9bc53de4386a26748edd
Devin Weaver <suki@tritarget.org>
2020-11-19 22:42:13 -0500

    Updates

---
+++ min/tracked-dom.js
@@ -0,0 +1,6 @@
+/**********************************************\
+*  FancyPants — customElements micro-lib   O   *
+*  MIT — Copyright © 2020 Devin Weaver    /|\  *
+*  https://sukima.github.io/fancy-pants/  </>  *
+\**********************************************/
+import dom from"./simple-dom.js";import{memoizeFunction}from"./tracking.js";export default dom.factory(memoizeFunction);
