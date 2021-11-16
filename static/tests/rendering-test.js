ad86959397745c136e8f8792365b4a8ead3fa163
Devin Weaver <suki@tritarget.org>
2021-11-16 16:51:28 -0500

    Add tests suite
    Had to guard against the yieldableProxy being triggered for non-string
    properties. This happened when using SinonJS for inspection of spy
    arguments. It used Symbol.toStringTag for inspection which was not the
    intent of the yieldableProxy. Others could run into this if they console
    logged or other manipulations to the yieldableProxy. This it was best to
    only react if the use case was a real property I.E. a string.

---
+++ static/tests/rendering-test.js
@@ -0,0 +1,30 @@
+/* globals QUnit sinon */
+import {
+  registerRenderer,
+  unregisterRenderer,
+  scheduleRender
+} from '../rendering.js';
+
+const { module, test } = QUnit;
+const nextMicroTask = () => Promise.resolve();
+
+module('rendering', function () {
+  test('schedules one rendering cycle at a time', async function () {
+    let mockRenderer = sinon.spy();
+
+    registerRenderer(mockRenderer);
+
+    try {
+      scheduleRender();
+      scheduleRender();
+      scheduleRender();
+      scheduleRender();
+      scheduleRender();
+      await nextMicroTask();
+    } finally {
+      unregisterRenderer(mockRenderer);
+    }
+
+    sinon.assert.calledOnce(mockRenderer);
+  });
+});
